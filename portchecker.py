e302d993f82567a1240196062b8f6e9f166680fc
bastosgabriel <bastos.gabriel@poli.ufrj.br>
2020-03-14 17:25:57 -0300

    Add colors to terminal messages

--- portchecker.py
+++ portchecker.py
@@ -4,9 +4,12 @@ import socket
 # Internet domain notation like 'daring.cwi.nl' or an IPv4 address like '100.50.200.5'
 # SOCK_STREAM: The default socket type. Uses a TCP protocol connection
 
+def printGreen(text): print(f"\033[92m {text}\033[00m") 
+def printRed(text): print(f"\033[91m {text}\033[00m") 
+
 try: 
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
-    s.settimeout(5)
+    s.settimeout(3)
     print("Socket created.")
 except socket.error as err: 
     print(f"Socket creation failed: {err}")
@@ -27,8 +30,10 @@ while True:
     port = int(input("Port: "))
 
     try:
-        print(f"Connecting to '{ip_or_name}' at port {port}")
+        print(f"Connecting to '{ip_or_name}'")
         s.connect((ip,port))
-        print(f"{ip}:{port} - Open")
+        print(f"{ip}:{port} - ", end='')
+        printGreen("Open")
     except socket.error:
-        print(f"{ip}:{port} - Closed")
+        print(f"{ip}:{port} - ", end='')
+        printRed("Closed")
